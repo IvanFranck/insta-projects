f136341d3f92090a4cf58f75fd336308a516e82f
ivan nzima <nzimaivan007@gmail.com>
2023-10-02 11:19:35 +0100

    create the ranting and thanks components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <script setup>
+import AppRating from './components/AppRating.vue';
+import ThankYouState from './components/ThankYouState.vue';
+
 </script>
 
 <template>
-  <div>hello world</div>
+  <div class="container">
+    <AppRating/>
+    <ThankYouState/>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.container{
+  min-height: 100vh;
+  width: 100%;
+  display: grid;
+  justify-items: center;
+  align-items: center;
+  padding: 0 20px;
+}
+</style>
